62025df1c885a3e99c1210c1422dbf22ce34028c
Excaliburrrr <kirito331789567@gmail.com>
2020-02-19 09:25:02 +0800

    Update miniweb

--- miniweb/dynamic/mini_frame.py
+++ miniweb/dynamic/mini_frame.py
@@ -1,19 +1,107 @@
 import time
 from register import *
+from util_sql import Stocks
+from urllib.parse import unquote
 import re
-import pymysql as pym
+import logging
 
 
+def get_logger():
+    """log日志的设置"""
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
 
-@route('/index.html')
-def index():
-    cnet = pym.connect(host='localhost', port=3306, user='root',
-                       password='654232', database='stock_db', charset='utf8')
-    cusr = cnet.cursor()
-    cusr.execute("SELECT * FROM info;")
-    stock_infos = cusr.fetchall()
-    cusr.close()
-    cnet.close()
+    logfile = "./log.txt"
+    fh = logging.FileHandler(logfile, mode='a')
+    fh.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.WARNING)
+
+    fomatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    fh.setFormatter(fomatter)
+    ch.setFormatter(fomatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
+
+
+@route(r'/add/(\d+)\.html')
+def add(ret):
+    logger = get_logger()
+    stock = Stocks()
+    # 1、获取股票代码
+    stock_code = ret.group(1)
+    # 2、判断是否有这只股票，一般只有网站被爬的时候会出现
+    if not stock.exists(stock_code):
+       return "非法操作, 股票不存在!"
+    # 3、判断是否关注过
+    if stock.is_focus(stock_code):
+        return "该股票已关注，请勿再次关注"
+    # 4、添加关注
+    try:
+        stock.add_focus(stock_code)
+    except SystemError as ret:
+        return ret
+    else:
+        logger.info("用户关注了 %s 这支股票" %stock_code)
+    return "关注成功"
+
+
+@route(r'/del/(\d+)\.html')
+def del_focus(ret):
+    logger = get_logger()
+    stock = Stocks()
+    # 1、获取股票代码
+    stock_code = ret.group(1)
+    # 2、判断是否关注过，如果没有则是非法请求
+    if not stock.is_focus(stock_code):
+        return "非法操作，未关注该股票！"
+    # 3、取消关注
+    try:
+        stock.del_focus(stock_code)
+    except SystemError as ret:
+        return ret
+    else:
+        logger.info("用户取消了 %s 这支股票" %stock_code)
+    return "取消成功"
+
+@route(r'/update/(\d+)\.html')
+def show_update_page(ret):
+    stock = Stocks()
+    # 1. 获取股票代码
+    stock_code = ret.group(1)
+    # 2. 获取对该股票的备注信息
+    note_info = stock.get_note_info(stock_code)
+    with open("./templates/update.html", "r") as f:
+        content = f.read()
+
+    content = re.sub(r'\{%code%\}', stock_code, content)
+    content = re.sub(r'\{%note_info%\}',  note_info, content)
+    return content
+
+@route(r'/update/(\d+)/(.*)\.html')
+def update_note(ret):
+    logger = get_logger()
+    stock = Stocks()
+    # 1. 获取股票代码, 与提交的备注修改
+    stock_code = ret.group(1)
+    # 对new_note进行url解码
+    new_note = unquote(ret.group(2))
+    # 2. 对该股票的备注进行修改
+    try:
+        stock.update_note(stock_code, new_note)
+    except SystemError as ret:
+        return ret
+    else:
+        logger.info("用户为 %s 这支股票添加了备注：%s" %(stock_code, new_note))
+    return "修改成功 %s" %new_note
+
+@route(r'/index.html')
+def index(ret):
+    stock = Stocks()
+    stock_infos = stock.get_stock_infos()
 
     html = ""
     ht_template = """<tr>
@@ -26,13 +114,13 @@ def index():
         <td>%s</td>
         <td>%s</td>
         <td>
-            <input type="button" value="添加" id="toAdd" name="toAdd" systemidvalue="000007">
+            <input type="button" value="添加" id="toAdd" name="toAdd" systemIdValue=%s>
         </td>
     </tr>
     """
     for info in stock_infos:
-        html += ht_template %(info[0], info[1], info[2],
-            info[3], info[4], info[5], info[6], info[7])
+        html += ht_template %(info.id, info.code, info.short,
+            info.chg, info.turnover, info.price, info.highs, info.time, info.code)
 
     with open("./templates/index.html") as f:
         content = f.read()
@@ -42,20 +130,10 @@ def index():
     return content
 
 
-@route('/center.html')
-def center():
-    cnet = pym.connect(host='localhost', port=3306, user='root',
-                       password='654232', database='stock_db', charset='utf8')
-    cusr = cnet.cursor()
-    cusr.execute("""SELECT
-                 i.code, i.short, i.chg, i.turnover,
-                 i.price, i.highs, f.note_info 
-                 FROM focus AS f
-                 LEFT JOIN info AS i 
-                 ON i.id = f.info_id;""")
-    focus_infos = cusr.fetchall()
-    cusr.close()
-    cnet.close()
+@route(r'/center.html')
+def center(ret):
+    stock = Stocks()
+    focus_infos = stock.get_focus_infos()
 
     html = ""
     ht_template = """<tr>
@@ -67,17 +145,17 @@ def center():
         <td>%s</td>
         <td>%s</td>
         <td>
-            <a type="button" class="btn btn-default btn-xs" href="/update/300268.html"> <span class="glyphicon glyphicon-
+            <a type="button" class="btn btn-default btn-xs" href="/update/%s.html"> <span class="glyphicon glyphicon-
             star" aria-hidden="true"></span> 修改 </a>
         </td>
         <td>
-            <input type="button" value="删除" id="toDel" name="toDel" systemidvalue="300268">
+            <input type="button" value="删除" id="toDel" name="toDel" systemIdValue=%s>
         </td>
     </tr>
     """
     for info in focus_infos:
-        html += ht_template %(info[0], info[1], info[2], info[3],
-                              info[4], info[5], info[6])
+        html += ht_template %(info.code, info.short, info.chg, info.turnover,
+                              info.price, info.highs, info.note_info, info.code, info.code)
     with open("./templates/center.html") as f:
         content = f.read()
     content = re.sub(r'\{%content%\}', html, content)
@@ -89,7 +167,14 @@ def application(environ, start_response):
     file_name = environ["PATH_INFO"]
     start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
     try:
-        return URL_MODULE_DICT[file_name]()
+        for url, call_module in URL_MODULE_DICT.items():
+            print(url)
+            ret = re.match(url, file_name)
+            if ret:
+                return call_module(ret)
+            else:
+                continue
+        return "请求的url(%s)没有对应的函数进行处理" %file_name
     except Exception as ret:
         return "产生了异常: %s" %ret
 
